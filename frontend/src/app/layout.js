93429afd170439017969f9415e2e50e7eb603512
Zachary Cook <zachary.cook@Zacharys-MacBook-Pro.local>
2023-09-24 17:23:07 -0400

    move fonts to static folder

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -2,7 +2,7 @@ import './globals.css'
 
 export const metadata = {
   title: 'Zscaler Automation Application',
-  description: 'Application built to help automate common tasks within Zscaler by working with the ZPA API.',
+  description: 'Application built to help automate common tasks within Zscaler by working with the ZPA API. Login Page',
 }
 
 export default function RootLayout({ children }) {
