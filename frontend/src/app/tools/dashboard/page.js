623258a26f15b79aee78c2bf767d5677b4f99c5d
zcook04 <zachary.cook@Zacharys-MacBook-Pro.local>
2023-09-26 17:00:03 -0400

    Add utility descriptions to dashboard

--- frontend/src/app/tools/dashboard/page.js
+++ frontend/src/app/tools/dashboard/page.js
@@ -9,8 +9,8 @@ const Home = () => {
         <section className={styles.utilitiesSection}>
             <h2>Utility Descriptions</h2>
             <DashboardSection title="Bulk Add" href="/tools/bulk-add" description="Add applications in bulk, up to 1000 at a time, to a preconfigured application segment." />
-            <DashboardSection title="Bulk Delete" href="/tools/bulk-add" description="Delete applications in bulk, up to 1000 at a time, from a preconfigured application segment." />
-            <DashboardSection title="Bulk Move" href="/tools/bulk-add" description="Move applications in bulk, up to 1000 at a time, from a preconfigured application segment to another preconfigured application segment." />
+            <DashboardSection title="Bulk Delete" href="/tools/bulk-delete" description="Delete applications in bulk, up to 1000 at a time, from a preconfigured application segment." />
+            <DashboardSection title="Bulk Move" href="/tools/bulk-move" description="Move applications in bulk, up to 1000 at a time, from a preconfigured application segment to another preconfigured application segment." />
         </section>
     )
 }
