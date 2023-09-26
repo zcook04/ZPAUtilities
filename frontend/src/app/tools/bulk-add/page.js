8e49d9d025d3a5782712960ae99adc9e3e393d16
zcook04 <zachary.cook@Zacharys-MacBook-Pro.local>
2023-09-26 11:39:33 -0400

    parse application list and remove duplicates.  prep for backend api fetch

--- frontend/src/app/tools/bulk-add/page.js
+++ frontend/src/app/tools/bulk-add/page.js
@@ -29,25 +29,30 @@ const BulkAddPage = () => {
     }, [])
 
     const submitHandler = async () => {
-        console.log('Adding Applications')
-    }
+        //split applications into an array of strings
+        const applicationArray = applications.split('\n')
+        //remove empty strings from array
+        const filteredApplicationArray = applicationArray.filter(application => application.length > 0)
+        //remove duplicate FQDNs and IPv4 addresses from array
+        const uniqueApplicationArray = [...new Set(filteredApplicationArray)]
+        console.log(uniqueApplicationArray)
 
-    console.log(selectedAppSegment)
+    }
 
     return (
         <section className={styles.summarySection}>
             <h2>Add Bulk Applications</h2>
             <p>When provided with a configured application segment, this tool can efficiently append a list of up to 1000 applications to it. These applications should meet the following criteria: they must not be already configured within another application segment, and they should exclusively comprise Fully Qualified Domain Names (FQDNs) and valid IPv4 addresses.</p>
-            <p>To proceed, please select the desired application from the dropdown menu below, and then paste the list of FQDNs and IPv4 addresses, separated by newlines. An illustrative example of the expected input format is displayed in the text box below for reference.</p>
+            <p>To proceed, please select the desired application from the dropdown menu below, and then paste the list of FQDNs and IPv4 addresses, separated by newlines.</p>
 
             <select placeholder='Select An Application Segment' name='applicationSegment' onChange={(e) => setSelectedAppSegment(e.target.value)} value={selectedAppSegment}>
                 <option value=''>Select An Application Segment</option>
                 {appSegments.map(appSegment => <option key={appSegment.id} value={appSegment.id}>{appSegment.name}</option>
                 )}
             </select>
-            <textarea placeholder='Paste FQDNs and IPv4 addresses here' name='applications'></textarea>
-            <div className={styles.button} onClick={submitHandler} >Add To Application Segment</div>
-        </section>
+            <textarea placeholder='Paste FQDNs and IPv4 addresses here.  Seperate each entry with a newline.' name='applications' value={applications} onChange={e => setApplications(e.target.value)}></textarea>
+            <div className={styles.button} onClick={submitHandler} >Add Applications To Application Segment</div>
+        </section >
     )
 }
 
