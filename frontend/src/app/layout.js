753c12b004f5e9148b388b13a0d7016d705b8889
Zachary Cook <zcook04@gmail.com>
2023-09-27 12:14:31 -0400

    Add Roboto font as variable font

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -2,6 +2,13 @@ import './globals.scss'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
+import { Roboto_Serif } from 'next/font/google'
+
+const roboto = Roboto_Serif({
+  subsets: ['latin'],
+  display: 'swap'
+})
+
 export const metadata = {
   title: 'Zscaler Automation Application',
   description: 'Application built to help automate common tasks within Zscaler by working with the ZPA API. Login Page',
@@ -10,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 
   return (
-    <html lang="en">
+    <html lang="en" className={roboto.className}>
       <body>
         {children}
         <ToastContainer />
