bef209e943069e5aadcf3a685f6a3fa4f414f0ee
zcook04 <zcook04@gmail.com>
2023-09-27 10:00:29 -0400

    Add frontend fetch of access-policy data

--- frontend/src/app/tools/access-policy/page.js
+++ frontend/src/app/tools/access-policy/page.js
@@ -5,12 +5,40 @@ import styles from './page.module.scss'
 import PageSummary from '@/components/pagesummary/PageSummary'
 
 const AccessPolicy = () => {
+    const [accessPolicy, setAccessPolicy] = useState({})
+
+    useEffect(() => {
+        const getAccessPolicy = async () => {
+            try {
+                const response = await fetch('/api/access-policy', {
+                    method: 'GET'
+                })
+                if (response.ok) {
+                    const data = await response.json()
+                    setAccessPolicy(data.accessPolicy)
+                }
+
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        getAccessPolicy()
+    }, [])
+
+    console.log(accessPolicy)
+
+
     return (
-        <section className={styles.summarySection}>
-            <PageSummary
-                title='Access Policy Analyser'
-                description="View, analyse and edit access policies for your application segments." />
-        </section>
+        <>
+            <section className={styles.summarySection}>
+                <PageSummary
+                    title='Access Policy Analyser'
+                    description="View and analyse access policies for your application segments." />
+            </section>
+            <section className={styles.policyViewerSection}>
+                <h2>Access Policy</h2>
+            </section>
+        </>
     )
 }
 
